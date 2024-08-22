3d5611da170b9188e8e8feb0283edd761a4406e8
mahi <Megha.Kumari@metropolia.fi>
2024-08-22 12:03:49 +0530

    self study file

---
+++ self study file.py
@@ -0,0 +1,57 @@
+name=1234565677687
+print(name)
+
+#/n ( paragraph )
+#print("hello\nteachers")
+
+# The program will not work unless the code is written with a required EXACT manner. put  quotation for string
+#input
+name= input("what is your name?")
+print("my name is " + name )
+
+name= "mahi"
+print("i am "+  name)
+
+# in another method:
+name=("mahi")
+print("i am" , name)
+
+#integer float string
+#int=7, float=7.5, string="ghsgh123456"
+
+name=input("location in Finland")
+name=(name + "i live in Myyrmaki in Finland") # we have to give quotations only for strings not for the varble.
+print(name) # if we put quotation in name then itb shows string and output will be also name not the input so quottion only for strings.
+
+#we can combine strings and numbers by using f-strings
+#name=int("mahi") by using int we can not give quotations as it is string.
+age=75 # as it is integer so quotation mark in not written.
+name= f"the age is {age}"
+print(name)
+print(f"the price of car is {20*67}")
+car=78
+#print( "the price of car is " + car   )# it is not performing because string and integer is not operated
+#if the integer lies in quotation then it will assume as string as will come as output but no operative .
+
+car= 78
+print(f"the price of the car is {car}")
+
+number=6/7
+print(f"the number is {number:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
